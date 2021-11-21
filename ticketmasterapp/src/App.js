84ec392ce623fd263a9c951760ce5198b4708296
Weston DeVore <westondevorebusiness@gmail.com>
2021-11-21 12:47:55 -0500

    moved everything to ticketmasterapp file

--- ticketmasterapp/src/App.js
+++ ticketmasterapp/src/App.js
@@ -1,66 +1,13 @@
 import React from 'react';
 import './App.css';
-// import ticketmasterapp from './ticketmasterapp';
-import { //1
-  Button,
-} from 'reactstrap';
-
-// let apiKey = "yzL8VYaOPxx1ke0AaJG7AISiofBfVrri";
-// let CustomerSecret = "YuiL1lgCdeR6L4bA";
-
-
-// const baseURL =  `https://app.ticketmaster.com//discovery/v2/events?apikey=${apiKey}`;
-// const baseImageURL = `https://app.ticketmaster.com/discovery/v2/events/{id}/images?apikey=${apiKey}`
-
-// const fetchAPI = () => {
-//     fetch(url)
-//   //     , 
-//   //     {
-//   //     mode: "no-cors"
-//   // })
-//   .then(res => res.json())
-//   .then(data => displayEvents(data))
-//   .catch(err => console.log(err))
-//   console.log('Line: 20');
-//   console.log(json.latlong)
-// };
-
-// let displayEvents = data => {
-//   console.log(data);
-//   const eventData = (data);
-//   console.log(searchTermData);
-
-//   const eventLatLongData = (eventData.latlong); //const converstionRate = (searchTermData.conversion_rates.USD);
-//   console.log(eventLatLongData);
-//   // const jsonConverstionRate = JSON.stringify(converstionRate);
-//   // document.getElementById("resultDisplay").innerHTML = `${jsonConverstionRate}`;
-// }
+import ticketmasterapp from './ticketmasterapp';
 
 function App() {
-    // const Locator = (position) => {
-    //   console.log(position);
-    // }
-    // navigator.geolocation.getCurrentPosition(Locator);
-    // let LatLong = Locator;
-    
-    // fetch(baseURL)
-
-  function fetchAPI () {
-    console.log('CLICK');
-  };
-      
-  return(
+  return (
     <div>
-      <h1>click me!</h1>
-      <Button onClick={() => fetchAPI}>button</Button>
-
+      <ticketmasterapp />
     </div>
   );
-
-  }
-
-
-  
-    
+}
 
 export default App;
--- ticketmasterapp/src/ticketmasterapp.js
+++ ticketmasterapp/src/ticketmasterapp.js
@@ -12,6 +12,39 @@ const baseURL =  `https://app.ticketmaster.com//discovery/v2/events?apikey=${api
 const baseImageURL = `https://app.ticketmaster.com/discovery/v2/events/{id}/images?apikey=${apiKey}`
 
 
+// let apiKey = "yzL8VYaOPxx1ke0AaJG7AISiofBfVrri";
+// let CustomerSecret = "YuiL1lgCdeR6L4bA";
+
+
+// const baseURL =  `https://app.ticketmaster.com//discovery/v2/events?apikey=${apiKey}`;
+// const baseImageURL = `https://app.ticketmaster.com/discovery/v2/events/{id}/images?apikey=${apiKey}`
+
+// const fetchAPI = () => {
+//     fetch(url)
+//   //     , 
+//   //     {
+//   //     mode: "no-cors"
+//   // })
+//   .then(res => res.json())
+//   .then(data => displayEvents(data))
+//   .catch(err => console.log(err))
+//   console.log('Line: 20');
+//   console.log(json.latlong)
+// };
+
+// let displayEvents = data => {
+//   console.log(data);
+//   const eventData = (data);
+//   console.log(searchTermData);
+
+//   const eventLatLongData = (eventData.latlong); //const converstionRate = (searchTermData.conversion_rates.USD);
+//   console.log(eventLatLongData);
+//   // const jsonConverstionRate = JSON.stringify(converstionRate);
+//   // document.getElementById("resultDisplay").innerHTML = `${jsonConverstionRate}`;
+// }
+
+
+
 // let displayEvents = data => {
 //   console.log(data);
 //   const eventData = (data);
@@ -22,8 +55,15 @@ const baseImageURL = `https://app.ticketmaster.com/discovery/v2/events/{id}/imag
 //   // document.getElementById("resultDisplay").innerHTML = `${jsonConverstionRate}`;
 // }
 
+  // const Locator = (position) => {
+  //   console.log(position);
+  // }
+  // navigator.geolocation.getCurrentPosition(Locator);
+  // let latlong = Locator;
+
+
 function ticketmasterapp() {
-  
+
 function fetchAPI() {
     fetch(baseURL)
     .then(res => res.json())
